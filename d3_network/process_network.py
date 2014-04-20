faf8bdfc40f3e532f1cb6fb2b1b63adbceab6e05
Samuel Lau <sclau@berkeley.edu>
2014-04-19 20:38:00 -0700

    Network Top 5 w/ selection

--- d3_network/process_network.py
+++ d3_network/process_network.py
@@ -4,8 +4,8 @@ import operator
 # get users, and unique user & business id's
 def get_data():
     users = {}
-    business_ids = []
-    with open('modified_users.txt', 'rb') as infile:
+    business_id = []
+    with open('../stat133proj2/processed_data/onlyfood_users.txt', 'rb') as infile:
         data = json.load(infile)
         for user in data.keys():
             businesses = data[user]['reviews'].keys()
@@ -14,15 +14,15 @@ def get_data():
                 if data[user]['reviews'][business]['city'] == "Berkeley":
                     user_business.append(business)
                     users[user] = user_business
-                    if business not in business_ids: 
-                        business_ids.append(business)
-    user_ids = users.keys()
-    return users, user_ids, business_ids
+                    if business not in business_id: 
+                        business_id.append(business)
+    user_id = users.keys()
+    return users, user_id, business_id
 
 # get top-x businesses in a city
 def business_topx():
-    top = 1
-    (users, user_ids, business_ids) = get_data()
+    top = 5
+    (users, user_id, business_id) = get_data()
     businesses = []
     business_count = {}
     for user in users.keys():     
@@ -34,10 +34,9 @@ def business_topx():
             business_count[business] = businesses.count(business)
     sorted_x = sorted(business_count.iteritems(), key=operator.itemgetter(1), reverse=True)
     top_x = sorted_x[0:top]
-    business_ids = []; business_reviews = []
+    business_ids = {}
     for x in xrange(0,top):
-        business_ids.append(top_x[x][0])
-        business_reviews.append(top_x[x][1])
+        business_ids[(top_x[x][0])] = top_x[x][1]
     # filter out non-top reviews from users    
     new_users = {}
     for user in users.keys():
@@ -48,55 +47,92 @@ def business_topx():
                 new_users[user] = user_biz
     users = new_users
     user_ids = users.keys()
-    return users, user_ids, business_ids, business_reviews
+    return users, user_ids, business_ids
 
 
 # reindex user id's for displaying
 def reid_ids():
-    (users, user_ids, business_ids, business_reviews) = business_topx()
-    user_dict = {}
-    for x in xrange(0,len(user_ids)):
-        user_dict[user_ids[x]] = x
-        user_ids[x] = x
-    business_dict = {}
-    for x in xrange(0,len(business_ids)):
-        business_dict[business_ids[x]] = x + len(user_ids)
-        business_ids[x] = x + len(user_ids)
+    (users, user_ids, business_ids) = business_topx()
+    user_id = user_ids
+    brev_dict = business_ids
+    bid_list = business_ids.keys()
+    """
+    This is the structure we want 
+        user_ids = 
+        { user_id":
+          "re_id":reindex_id,
+          "user_name":user_name,
+          "user_reviews":user_reviews},...
+        }
+        business_ids = 
+        { business_id:
+          "re_id":reindex_id,
+          "bus_name":business_name
+          "bus_reviews":business_reviews},...
+        }
+    """
+    user_ids = {}
+    business_ids = {}
+    for user in user_id:
+        user_ids[user] = {'re_id':'','name':'','reviews':''}
+    for business in bid_list:
+        business_ids[business] = {'re_id':'','name':'','reviews':''}
+    # user and business re-index
+    for x in xrange(0,len(user_id)):
+        user = user_id[x]
+        user_ids[user]['re_id'] = x
+        
+    for x in xrange(0,len(bid_list)):
+        business_ids[bid_list[x]]['re_id'] = x + len(user_id)
+        
+    # user and business name index    
+    with open('data/yelp_academic_dataset.json', 'rb') as f:
+        for line in f:
+            item = json.loads(line)
+            if item['type'] == "user":
+                for user in user_id:
+                    if item['user_id'] == user:
+                        user_ids[user]['name'] = item['name']
+            elif item['type'] == "business":
+                for business in bid_list:
+                    if item['business_id'] == business:
+                        business_ids[business]['name'] = item['name']
+                        
+    # user reviews in sample index
     for user in users.keys():
-        for i,j in user_dict.iteritems():
-            if user == i: users[j] = users.pop(user)    
-	# replace business items 
-    for user in users.keys():
-        for y in xrange(0,len(users[user])):
-            for i,j  in business_dict.iteritems():
-                if users[user][y] == i: users[user][y] = j
-    return users, user_ids, business_ids, business_reviews
+        user_ids[user]['reviews'] = len(users[user])
+                
+    for business in bid_list:
+        business_ids[business]['reviews'] = brev_dict[business]
+                
+    return users, user_ids, business_ids
 
 # create nodes for each user and business, sized by review count
 def create_nodes():
+    (users, user_ids, business_ids) = reid_ids()
     nodes = []
-    (users, user_ids, business_ids, business_reviews) = reid_ids()
-    for user in user_ids:
-        nodes.append({"person":user, "reviewcount":len(users[user])})
+    for user in user_ids.keys():
+        nodes.append({"_name":user_ids[user]['name'], "user_reviewcount":user_ids[user]['reviews']})
     i = 0 
-    for business in business_ids:
-        nodes.append({"person":business, "reviewcount":business_reviews[i]})
+    for business in business_ids.keys():
+        nodes.append({"_name":business_ids[business]['name'], "bus_reviewcount":business_ids[business]['reviews']/10})
         i += 1
     return nodes
 
 # create links from user to businesses
 def create_links():
+    (users, user_ids, business_ids) = reid_ids()
     links = []
-    (users, user_ids, business_ids, business_reviews) = reid_ids()
-    for user in user_ids:
+    for user in users.keys():
         for business in users[user]:
-            links.append({"source": user, "target": business})
+            links.append({"source": user_ids[user]['re_id'], "target": business_ids[business]['re_id']})
     return links
 
 def main():
 	new_data = {"nodes":create_nodes(), "edges":create_links()}
-	with open('data.json', 'w') as outfile:
+	with open('data/Berkeley.json', 'w') as outfile:
 		json.dump(new_data, outfile, indent=2)
 
 if __name__=='__main__':
 	main()
+
--- d3_network/script.js
+++ d3_network/script.js
@@ -1,57 +1,88 @@
-// Width and height
-var w = 900;
-var h = 700;
+var w = 800
+var h = 700
+var	r = 6
 
-// Load Data
-d3.json("data.json",function(dataset){
 
-	// Initialize a default force layout, using the nodes and edges
-	var force = d3.layout.force()
-		.nodes(dataset.nodes)
-		.links(dataset.edges)
-		.size([w, h])
-		.linkDistance([10])
-		.charge([-100])
-		.start();
+function network(data)
+{
+	d3.json("data/" + data, function(dataset){
+		d3.select("svg").remove()
+		// Initialize a default force layout, using the nodes and edges
+		var force = d3.layout.force()
+			.nodes(dataset.nodes)
+			.links(dataset.edges)
+			.size([w, h])
+			.linkDistance([10])
+			.charge([-50])
+			.start();
 
-	var colors = d3.scale.category10();
+		// Create SVG element
+		var svg = d3.select("body")
+			.append("svg")
+			.attr("width", w)
+			.attr("height", h);			
 
-	// Create SVG element
-	var svg = d3.select("body")
-		.append("svg")
-		.attr("width", w)
-		.attr("height", h);		
+		var colors = d3.scale.category20();
 
-	// Create edges as lines
-	var edges = svg.selectAll("line")
-		.data(dataset.edges)
-		.enter()
-		.append("line")
-		.style("stroke", "#ccc")
-		.style("stroke-width", 0.2);
+		// Create edges as lines
+		var edges = svg.selectAll("line")
+			.data(dataset.edges)
+			.enter()
+			.append("line")
+			.style("stroke", "#ccc")
+			.style("stroke-width", 0.2);
 			
-	// Create nodes as circles
-	var nodes = svg.selectAll("circle")
-		.data(dataset.nodes)
-		.enter()
-		.append("circle")
-		.attr("r", function(d){return d.reviewcount})
-		.style("fill", function(d,i){return colors(i);})
-		.call(force.drag);
+		// Create nodes as circles
+		var nodes = svg.selectAll("circle")
+			.data(dataset.nodes)
+			.enter()
+			.append("circle")
+			.attr("r", function(d){
+				if (d.user_reviewcount){return d.user_reviewcount}
+				else if (d.bus_reviewcount){ 
+					if (d.bus_reviewcount < 1000){return d.bus_reviewcount}
+					else {return d.bus_reviewcount/10}
+				}
+			})
+			.style("fill", function(d,i){return colors(i);})
+			.call(force.drag);
 				
-	// Draw text on the screen
-	nodes.append("title")
-		.data(dataset.nodes)
-		.text(function(d){return d.person});
+		// Draw text on the screen, 
+		nodes.append("title")
+			.data(dataset.nodes)
+			.text(function(d){
+				content = d._name
+				if (d.bus_reviewcount){content += '\nReview Count: ' + d.bus_reviewcount*10}
+				return content});
+		
+		// Every time the simulation "ticks", this will be called
+		force.on("tick", function() {
+			nodes.attr("cx", function(d) { return d.x = Math.max(r, Math.min(w - r, d.x)); })
+				.attr("cy", function(d) { return d.y = Math.max(r, Math.min(h - r, d.y)); });
 			
-	// Every time the simulation "ticks", this will be called
-	force.on("tick", function() {
-		edges.attr("x1",function(d){return d.source.x;})
-				.attr("y1",function(d){return d.source.y;})
-				.attr("x2",function(d){return d.target.x;})
-				.attr("y2",function(d){return d.target.y;});
-		nodes.attr("cx",function(d){return d.x;})
-				.attr("cy",function(d){return d.y;});
-	
+			edges.attr("x1", function(d) { return d.source.x; })
+				.attr("y1", function(d) { return d.source.y; })
+				.attr("x2", function(d) { return d.target.x; })
+				.attr("y2", function(d) { return d.target.y; });
 		});
-});
+	});
+
+};
+
+function init()
+{
+    d3.select("#Berkeley")
+        .on("click", function(d,i) {
+            network('Berkeley.json')
+        })   
+    d3.select("#NewYork")
+        .on("click", function(d,i) {
+            network('New_York.json')
+        })   
+    d3.select("#LosAngeles")
+        .on("click", function(d,i) {
+            network('Los_Angeles.json')
+        })
+    //make the network
+    network('Berkeley.json')
+};
