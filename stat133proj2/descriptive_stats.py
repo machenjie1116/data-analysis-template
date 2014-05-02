9b397877c092de9670058209f0996a981ce2a800
Samuel Lau <sclau@berkeley.edu>
2014-05-02 16:17:04 -0700

    Update descriptive_stats.py

--- stat133proj2/descriptive_stats.py
+++ stat133proj2/descriptive_stats.py
@@ -1,6 +1,7 @@
 import json
 import csv
 
+# businesses from our sample
 with open('onlyfood_users.txt', 'rb') as f:
     businesses = {}
     data = json.load(f)
@@ -11,6 +12,7 @@ with open('onlyfood_users.txt', 'rb') as f:
             if business not in businesses:
                 businesses[business] = {'city':city, 'state':state}
 
+# get business info from full dataset
 with open('data/yelp_academic_dataset.json') as f1:
 	data_out = {}
 	for line in f1:
@@ -25,22 +27,12 @@ with open('data/yelp_academic_dataset.json') as f1:
                                          'state':businesses[business_id]['state']
                                          }
 
-with open('data/business_table.csv', 'w') as f:
-    csv_file = csv.writer(f)
-    for business in data_out.keys():
-        name = data_out[business]['name']
-        reviews = data_out[business]['review_count']
-        csv_file.writerow([business,
-                           name.encode('utf8'),
-                           reviews,
-                           data_out[business]['rating'],
-                           data_out[business]['city'],
-                           data_out[business]['state']])
-
+# encode business names for csv
 for business in data_out.keys():
     name = data_out[business]['name']
     name.encode('utf8')
 
+# write to csv business data
 with open('data/business_table.csv', 'w') as f:
     csv_file = csv.writer(f)
     for business in data_out.keys():
